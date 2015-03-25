b23175cb02fbde73645a1dcd953253cc6652f9bc
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-25 14:39:19 -0500

    almost finish pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.Size = 0;
+
+  Queue.prototype.constructor = Queue;
+
 };
+
+Queue.prototype = {
+  enqueue: function(value) {
+    this.storage[this.Size] = value;
+    this.Size++;
+  },
+
+  dequeue: function(){
+    this.Size && this.Size--;
+    var result = this.storage[0];
+    this.storage[0] = this.storage[1]
+    return result;
+  },
+  size: function() {
+    return this.Size;
+  }
+};
+
+
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+
+  Stack.prototype.constructor = Stack;
+};
+
+Stack.prototype = {
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop: function() {
+    this.count && this.count--;
+    var result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  },
+  size: function() {
+    return this.count;
+  }
 };
 
 
