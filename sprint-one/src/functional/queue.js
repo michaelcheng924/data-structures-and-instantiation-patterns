eafe19175dde6bf37fccc2defd85976fb8ed39f9
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-25 13:49:45 -0500

    finish stack functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,6 +4,9 @@ var Queue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var start = 0;
+  var end = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value){
