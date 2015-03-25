1ad3640c4a912dfca6eda9f4d56a02fe83af461b
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-25 14:03:16 -0500

    finish queue functional shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,32 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var queue = {};
+
+  queue.storage = {};
+  queue.Size = 0;
+
+  _.extend(queue, queueMethods);
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.Size] = value;
+    this.Size++;
+  },
+  dequeue: function() {
+    this.Size && this.Size--;
+    var result = this.storage[0];
+    this.storage[0] = this.storage[1];
+    return result;
+  },
+  size: function() {
+    return this.Size;
+  }
+};
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+
+  this.storage = {};
+  this.count = 0;
 };
 
 var stackMethods = {};
