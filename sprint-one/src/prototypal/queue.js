5122bd1bb407052e7e38ed277ba6a54a8d8d23c8
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-26 11:46:11 -0500

    refactor queue prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -2,28 +2,28 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var queue = Object.create(queueMethods);
-  queue.storage = {};
-  queue.start = 0;
-  queue.end = 0;
+  queue._storage = {};
+  queue._start = 0;
+  queue._end = 0;
 
   return queue;
 };
 
 var queueMethods = {
   enqueue: function(value) {
-    this.storage[this.end] = value;
-    this.end++;
+    this._storage[this._end] = value;
+    this._end++;
   },
   dequeue: function()  { 
-  var result = this.storage[this.start];
+  var result = this._storage[this._start];
   if (this.size()){
-    delete this.storage[this.start];
-    this.start++;
+    delete this._storage[this._start];
+    this._start++;
   }
   return result;
   },
   size: function() {
-    return this.end - this.start;
+    return this._end - this._start;
   }
 };
 
