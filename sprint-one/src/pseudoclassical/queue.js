54a327b96ed4e4f3da9fe1e1bd641eade43939f8
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-26 11:48:08 -0500

    refactor queue pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -2,26 +2,26 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 
-  this.storage = {};
-  this.start = 0;
-  this.end = 0;
+  this._storage = {};
+  this._start = 0;
+  this._end = 0;
 
 };
 
 Queue.prototype.enqueue = function(value) {
-  this.storage[this.end] = value;
-  this.end++;
+  this._storage[this._end] = value;
+  this._end++;
 };
 
 Queue.prototype.dequeue = function() {
-  var result = this.storage[this.start];
+  var result = this._storage[this._start];
   if (this.size()) {
-    delete this.storage[this.start];
-    this.start++;
+    delete this._storage[this._start];
+    this._start++;
   }
   return result;
 };
 
 Queue.prototype.size = function() {
-  return this.end - this.start;
+  return this._end - this._start;
 };
