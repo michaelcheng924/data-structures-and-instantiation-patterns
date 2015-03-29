45a5d860b75dfaa44cb8e8a476826a4275dd86e1
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-29 14:29:16 -0500

    add comments to linked list addToTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,16 +1,36 @@
 var LinkedList = function(){
+
+  // Functional instantiation pattern
   var list = {};
+
+  // Initialize head and tail values to null
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
-    if(this.head === null){
+    // If no head value exists
+    if (this.head === null){
+      // Create a new Node for head
       this.head = new Node(value);
+      // Set tail equal to head
       this.tail = this.head;
+    // If head exists
     } else {
+      // Set value after tail to a new Node
       this.tail.next = new Node(value);
+      // Move tail to be at the end
       this.tail = this.tail.next;
     }
+  };
+
+  list.addToHead = function(value){
+    // If no tail value exists
+    // if ()
+      // Create a new Node for tail
+      // Set head equal to tail
+    // If tail exists
+      // Set value before head to a new Node
+      // Move head to be at the front
   };
 
   list.removeHead = function(){
@@ -36,6 +56,7 @@ var Node = function(value){
 
   node.value = value;
   node.next = null;
+  node.previous = null;
 
   return node;
 };
