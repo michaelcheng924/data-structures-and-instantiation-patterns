d363f51fdacfa0c0f99d4ffd31f28fb1f8ea37e5
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-29 13:53:43 -0500

    add comments to binary search tree insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,12 @@
 var BinarySearchTree = function(value){
+
+  // Prototypal instantiation patter
   var tree = Object.create(BinarySearchTree.prototype);
+
+  // Value is used for comparisons
   tree.value = value;
+
+  // Initialize left and right values to null
   tree.left = null;
   tree.right = null;
 
@@ -8,13 +14,22 @@ var BinarySearchTree = function(value){
 };
 
 BinarySearchTree.prototype.insert = function(value){
+
+  // Recursively walk down tree to correct spot
   var walkTree = function(tree) {
+
+    // Compare inserted value with node value
     if (value > tree.value) {
+
+      // If value is greater and no right value exists
       if (!tree.right) {
+        // Set the right value to a new subtree
         tree.right = BinarySearchTree(value);
       } else {
+        // If right value exists, repeat walkTree with this node
         walkTree(tree.right);
       }
+    // Repeat if value is less than node value
     } else {
       if (!tree.left) {
         tree.left = BinarySearchTree(value);
@@ -23,8 +38,10 @@ BinarySearchTree.prototype.insert = function(value){
       }
     }
   };
+  // Initialize walkTree function with top node
   walkTree(this);
 };
+
 BinarySearchTree.prototype.contains = function(target){
   var contains = false;
 
